87dd03f01c166b0df9f99794a489120a684cecdf
Jordan Nunez <Jordan@Jordans-MacBook-Air.local>
2015-09-25 12:03:48 -0700

    grab distinct_id from cookie of the request

--- webserver.py
+++ webserver.py
@@ -1,11 +1,21 @@
 # Web server libraries
 from flask import Flask, redirect, render_template, request
+from urllib import unquote
+import json
 
 # Initiate web-app
 app = Flask(__name__)
 
 app.debug = True
 app.secret_key = 'development'
+
+MP_TOKEN = 'jordan'
+
+# Mixpanel cookie parser
+def cookie_to_mixpanel_data(cookie, token):
+    mp_cookie_name = "mp_%s_mixpanel" % token
+    mp_cookie = cookie[mp_cookie_name]
+    return json.loads(unquote(mp_cookie).decode('utf8'))
 
 # The app.route() wrapper handles request addresses coming in
 @app.route('/')
@@ -16,6 +26,7 @@ def index():
 @app.route('/registration.html', methods = ['GET', 'POST'])
 def registration():
     if request.method == "POST":
+        print cookie_to_mixpanel_data(request.cookies, MP_TOKEN)['distinct_id']
         return redirect('home.html')
     else:
         return render_template('registration.html')
@@ -23,6 +34,7 @@ def registration():
 @app.route('/home.html')
 def home():
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     # Starts the server on all IPs for the host on a port 8000
