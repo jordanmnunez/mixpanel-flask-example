ad0fb0978c1f9078a23891fa6513c416b1413891
Jordan Nunez <Jordan@Jordans-MacBook-Air.local>
2015-09-25 12:54:22 -0700

    make it more 'lifelike'
    runs from a fake database, user cant register twice

--- webserver.py
+++ webserver.py
@@ -1,8 +1,10 @@
 # Web server libraries
-from flask import Flask, redirect, render_template, request
-from urllib import unquote
-import json
+from flask import Flask, flash, redirect, render_template, request
+
 from mixpanel import Mixpanel
+import json
+from urllib import unquote
+import uuid
 
 # Initiate web-app
 app = Flask(__name__)
@@ -14,11 +16,33 @@ app.secret_key = 'development'
 MP_TOKEN = 'jordan'
 mixpanel = Mixpanel(MP_TOKEN)
 
+# Fake Database
+DB = {
+    'users': {}
+}
+
 # Mixpanel cookie parser
 def cookie_to_mixpanel_data(cookie, token):
     mp_cookie_name = "mp_%s_mixpanel" % token
-    mp_cookie = cookie[mp_cookie_name]
+    mp_cookie = cookie.get(mp_cookie_name)
     return json.loads(unquote(mp_cookie).decode('utf8'))
+
+# Fake function that would simulate writting a user to your dabatse
+def create_user(form, cookie):
+    if request.form['email'] not in DB['users']:
+        DB['users'][request.form['email']] = {
+            'alias': request.form['email'],
+            'distinct_id': cookie['distinct_id'],
+            '$first_name': request.form['fn'],
+            '$last_name': request.form['ln'],
+            '$email': request.form['email'],
+            'Favourite Genre': request.form['genre'],
+            'udid': str(uuid.uuid4()),
+        }
+
+        return DB['users'][request.form['email']]
+    else:
+        return None
 
 # The app.route() wrapper handles request addresses coming in
 @app.route('/')
@@ -30,18 +54,15 @@ def index():
 def registration():
     if request.method == "POST":
         cookie = cookie_to_mixpanel_data(request.cookies, MP_TOKEN)
-        user_info = {
-            'alias': request.form['email'],
-            'distinct_id': cookie['distinct_id'],
-            '$first_name': request.form['fn'],
-            '$last_name': request.form['ln'],
-            '$email': request.form['email'],
-            'Favourite Genre': request.form['genre'],
-            }
-        mixpanel.alias(user_info['alias'], user_info['distinct_id'])
-        mixpanel.track(user_info['distinct_id'], 'Signup', user_info, meta={'$ip':0})
-        mixpanel.people_set(user_info['distinct_id'], user_info, meta={'$ip':0, '$ignore_time':True})
-        return redirect('home.html')
+        user_info = create_user(request.form, cookie)
+        if user_info:
+            mixpanel.alias(user_info['alias'], user_info['distinct_id'])
+            mixpanel.track(user_info['distinct_id'], 'Signup', user_info, meta={'$ip':0})
+            mixpanel.people_set(user_info['distinct_id'], user_info, meta={'$ip':0, '$ignore_time':True})
+            return redirect('home.html')
+        else:
+            flash(u'This email already exists', 'error')
+            return redirect('registration.html')
     else:
         return render_template('registration.html')
 
